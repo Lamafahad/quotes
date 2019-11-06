4cf36a7f0c27d3bf66be52a7b2e74c1ad5caec24
Lamafahad <lamafa97@gmail.com>
2019-11-06 16:14:25 +0300

    Updates

---
+++ precache-manifest.40512292ca71c115cb30bf856564a4b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccd5ce813e7792243aeaddc640f9830",
+    "url": "/quotes/index.html"
+  },
+  {
+    "revision": "6c0c1eb288b0609641b0",
+    "url": "/quotes/static/css/main.818b9524.chunk.css"
+  },
+  {
+    "revision": "9e0cdac326f1524c1e1b",
+    "url": "/quotes/static/js/2.bc3149db.chunk.js"
+  },
+  {
+    "revision": "6c0c1eb288b0609641b0",
+    "url": "/quotes/static/js/main.881c4f31.chunk.js"
+  },
+  {
+    "revision": "5ef0273d94ed46ca3eb5",
+    "url": "/quotes/static/js/runtime-main.76811ed1.js"
+  }
+]);
